e14301d1b9880a9c262a04636e35758df6250e21
fredrikwidar <fredrikwidar@hotmail.se>
2024-09-26 09:27:37 +0200

    Update SignInSuccessMessage.tsx

--- app/sign-in/success/SignInSuccessMessage.tsx
+++ app/sign-in/success/SignInSuccessMessage.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import { useRouter } from "next/navigation";
+import AuthBackground from "@/app/my-components/AuthBackground";
 
 export default function SuccessMessage() {
   const router = useRouter();
@@ -15,11 +16,13 @@ export default function SuccessMessage() {
   }, [router]);
 
   return (
+    <AuthBackground>
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <h1 className="text-4xl font-bold mb-4">You are now signed in</h1>
         <p>Redirecting to home page...</p>
       </main>
     </div>
+    </AuthBackground>
   );
 }
