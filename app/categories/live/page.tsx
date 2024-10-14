ec15d116dfd1fc59ef95646d043d263a044123c7
EshaniHansika <eshanihansi@gmail.com>
2024-10-14 10:34:36 +0200

    done

--- app/categories/live/page.tsx
+++ app/categories/live/page.tsx
@@ -1,10 +1,15 @@
 import { PrismaClient } from '@prisma/client';
-import { Articles } from '@/app/front-page/types'; 
-import Link from 'next/link';
+import { getServerSession } from 'next-auth';
+import { Articles } from '@/app/front-page/types';
+import { ArticleCardLatestNews, ArticleCardPopularNews, ArticleCardEditorChoice } from '@/app/front-page/ArticleCard';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 
 const prisma = new PrismaClient();
 
 export default async function LiveNews() {
+    const session = await getServerSession(authOptions);
+    const userId = session?.user?.id || '';
+
     // Fetch Latest News in Live Category
     const latestLiveNews: Articles[] = await prisma.article.findMany({
         where: { category: {some: { name: 'Live' } }}, 
@@ -40,21 +45,8 @@ export default async function LiveNews() {
                     <div className="space-y-3">
                     {latestLiveNews.map((article) => (
                         <div key={article.id}>
-                            <Link href={`/article-page/${article.id}`}>
-                                <div>
-                                    <h3 className="font-bold text-xl text-black hover:underline">{article.headline}</h3>
-                                    <img
-                                    src={article.image}
-                                    alt={article.headline}
-                                    className="w-full object-cover mt-2"
-                                    />
-                                    
-                                    <p className="text-sm text-gray-800 whitespace-normal break-words pt-3">
-                                    {article.summary}
-                                    </p>
-                                    <hr className="my-4 border-gray-300" />
-                                </div>
-                            </Link>
+                            <ArticleCardLatestNews key={article.id} article={article} userId={userId} />
+
                         </div>
                     ))}
                     </div>
@@ -73,32 +65,20 @@ export default async function LiveNews() {
         <hr className="my-1 border-gray-500" /> 
 
         <section className="mt-12 p-5">
-              <h2 className="text-lg font-bold mb-6 text-gray-700">More</h2>
+            <h2 className="text-lg font-bold mb-6 text-gray-700">More</h2>
               <div className="grid grid-cols-2 gap-6 md:grid-cols-2 lg:grid-cols-3">
-                  {otherCategories.map((category) => (
-                      <div key={category.id} className="p-4">
-                          <h3 className="text-md font-bold text-gray-800 mb-2">{category.name}</h3>
-                          <div className="space-y-2">
-                              {category.articles && category.articles.length > 0 ? (
-                                  category.articles.map((article) => (
-                                      <div key={article.id} className="pb-2 mb-2">
-                                        <Link href={`/article-page/${article.id}`}>
-                                            <h4 className="text-sm font-semibold text-gray-900 hover:underline">{article.headline}</h4>
-                                            <div className="relative">
-                                                <img
-                                                    src={article.image}
-                                                    alt={article.headline}
-                                                    className="w-auto object-cover mt-2"
-                                                />
-                                            </div>
-                                            <p className="text-xs text-gray-600 pt-3">{article.summary}</p>
-                                          </Link>
-                                      </div>
-                                  ))
-                              ) : null}
-                          </div>
-                      </div>
-                  ))}
+                {otherCategories.map((category) => (
+                  <div key={category.id} className="p-4">
+                    <h3 className="text-md font-bold text-gray-800 mb-2">{category.name}</h3>
+                    <div className="space-y-2">
+                      {category.articles && category.articles.length > 0 ? (
+                        category.articles.map((article) => (
+                          <ArticleCardLatestNews key={article.id} article={article} userId={userId} />
+                        ))
+                      ) : null}
+                    </div>
+                  </div>
+                ))}
               </div>
             </section>
       </main>
