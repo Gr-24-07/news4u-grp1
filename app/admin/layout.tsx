0188b1824f28d818df90b3df895558176c019acf
rinnegard <57401904+rinnegard@users.noreply.github.com>
2024-10-16 09:27:38 +0200

    added separator and title to big screen admin nav

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,3 +1,4 @@
+import { Separator } from "@/components/ui/separator";
 import AdminNav from "./admin-nav";
 import DropdownAdminNav from "./dropdown-admin-nav";
 
@@ -8,7 +9,11 @@ export default function RootLayout({
 }>) {
     return (
         <div className="w-full flex flex-col lg:flex-row gap-6 my-0 lg:my-6 ">
-            <nav className="w-1/6 px-6 hidden lg:block">
+            <nav className="w-1/6 px-6 hidden lg:block space-y-2">
+                <h2 className="font-bold text-lg flex flex-row items-center">
+                    Menu
+                </h2>
+                <Separator />
                 <AdminNav />
             </nav>
             <DropdownAdminNav></DropdownAdminNav>
