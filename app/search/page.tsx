91bca59f377155e5b176177b3faf1da4f5ce5532
SidraMufti <imsidranaveed@gmail.com>
2024-10-18 10:22:24 +0200

    searchpage with daysAgo

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -2,8 +2,8 @@ import prisma from "@/lib/db";
 import React from "react";
 import ArticleSearch from "./search";
 import Image from "next/image";
-import { Card, CardContent } from "@/components/ui/card";
 import Link from "next/link";
+import { DateTime } from "luxon";
 
 export default async function SearchPage({
   searchParams,
@@ -23,7 +23,16 @@ export default async function SearchPage({
         mode: "insensitive",
       },
     },
+    include: {
+      category: {
+        select: {
+          name: true,
+        },
+      },
+    },
   });
+
+  const options = { zone: "UTC" };
 
   return (
     <div>
@@ -34,39 +43,57 @@ export default async function SearchPage({
         <ArticleSearch />
       </div>
 
-      <div className="transition-colors px-4 gap-2">
-        <Card className="border border-zinc-200 p-8">
-          <CardContent>
-            <div>
-              {articles.map((article) => (
-                <div className="flex flex-1" key={article.id}>
-                  <div className="flex flex-col w-2/3">
-                    <h2 className="text-xl font-bold hover:underline mb-3 ">
-                      <Link href={`/article-page/${article.id}`}>
-                        {article.headline}
-                      </Link>
-                    </h2>
-                    <p>
-                      <Link href={`/article-page/${article.id}`}>
-                        {article.summary}
-                      </Link>
-                    </p>
-                  </div>
-                  <div className="flex flex-1 w-1/3">
+      <div className="px-4 gap-2">
+        <div className="p-8">
+          <div>
+            {articles.map((article) => (
+              <div
+                className="grid grid-cols-2 border-b border-gray-300 mb-4"
+                key={article.id}
+              >
+                <div className=" ">
+                  <h2 className="text-xl font-bold hover:underline mb-3">
                     <Link href={`/article-page/${article.id}`}>
-                      <Image
-                        src={article.image}
-                        alt={article.headline}
-                        width={300}
-                        height={300}
-                      />
+                      {article.headline}
                     </Link>
-                  </div>
+                  </h2>
+                  <p>
+                    <Link href={`/article-page/${article.id}`}>
+                      {article.summary}
+                    </Link>
+                  </p>
                 </div>
-              ))}
-            </div>
-          </CardContent>
-        </Card>
+
+                <Link href={`/article-page/${article.id}`}>
+                  <Image
+                    src={article.image}
+                    alt={article.headline}
+                    width={900}
+                    height={315}
+                    className="mx-auto justify-end w-4/5"
+                  />
+                </Link>
+                <small className="flex justify-between text-muted-foreground mb-4">
+                  {article.category.map((category) => (
+                    <Link
+                      href={`/categories/${category.name.toLocaleLowerCase()}`}
+                      key={category.name}
+                      className="hover:underline text-underline-offset"
+                    >
+                      {category.name}
+                    </Link>
+                  ))}
+                  <time className="text-muted-foreground">
+                    {DateTime.fromJSDate(
+                      article.createdAt,
+                      options
+                    ).toRelative()}
+                  </time>
+                </small>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
