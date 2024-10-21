f07dbc1f905ecec787878a364fb2de9383a1cb78
rinnegard <57401904+rinnegard@users.noreply.github.com>
2024-10-21 15:57:11 +0200

    added data table pagination

--- app/admin/articles/data-table.tsx
+++ app/admin/articles/data-table.tsx
@@ -18,8 +18,8 @@ import {
     TableHeader,
     TableRow,
 } from "@/components/ui/table";
-import { Button } from "@/components/ui/button";
 import { useState } from "react";
+import { DataTablePagination } from "./data-table-pagination";
 
 interface DataTableProps<TData, TValue> {
     columns: ColumnDef<TData, TValue>[];
@@ -95,7 +95,8 @@ export function DataTable<TData, TValue>({
                     )}
                 </TableBody>
             </Table>
-            <div className="flex items-center justify-end space-x-2 py-4">
+            <DataTablePagination table={table}></DataTablePagination>
+            {/* <div className="flex items-center justify-end space-x-2 py-4">
                 <Button
                     variant="outline"
                     size="sm"
@@ -112,7 +113,7 @@ export function DataTable<TData, TValue>({
                 >
                     Next
                 </Button>
-            </div>
+            </div> */}
         </div>
     );
 }
